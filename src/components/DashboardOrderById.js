d079f981096be853985f894767d707511314c680
Adesola Olalekan <71498022+LakeCombs@users.noreply.github.com>
2023-05-17 11:31:53 +0100

    worked on all the curren dashboard screens

--- src/components/DashboardOrderById.js
+++ src/components/DashboardOrderById.js
@@ -1,13 +1,13 @@
-import { Radio } from "antd";
+import { Avatar, Button, Input, Radio, Space } from "antd";
 import React from "react";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { GoPrimitiveDot } from "react-icons/go";
 
 const DashboardOrderById = () => {
 	return (
 		<div className="bg-white  w-full p-[10px]">
-			<div
-				className="flex flex-row justify-between
-            ">
-				<div className="w-1/2 border outline-1 rounded-lg bg-background px-[10px] py-[15px]">
+			<div className="flex flex-row justify-between">
+				<div className="w-[70%] mr-[5px] border outline-1 rounded-lg bg-background px-[10px] py-[15px]">
 					<div className="flex flex-row w-3/4 justify-between">
 						<p className="font-bold text-[12px]">Order #000001</p>
 						<p className="bg-orange-400 px-[7px] py-[5px] w-[100px] text-center rounded-lg font-bold hover:cursor-pointer">
@@ -21,22 +21,193 @@ const DashboardOrderById = () => {
 						</span>{" "}
 						<span className="font-bold text-[12px]">Unfulfilled</span>
 					</p>
-					<div className="pb-[5px] border-b grid grid-cols-5 gap-1 text-[12px] mt-[12px]">
+					<div className="pb-[5px] border-b grid grid-cols-6 gap-1 text-[12px] mt-[12px]">
 						<p>Product</p>
 						<p>SKU</p>
 						<p>Quantity</p>
 						<p>Price</p>
 						<p>Total</p>
+						<p></p>
+					</div>
+
+					<div className="pb-[5px] border-b grid grid-cols-6 gap-1 text-[12px] mt-[12px] items-center">
+						<img
+							src="https://media.istockphoto.com/id/1396897706/photo/vanilla-soft-serve-ice-cream-cone.jpg?b=1&s=170667a&w=0&k=20&c=S6oypYSoesaaKrndBk1POlIVhojg4WIv3Br0eplLuoA="
+							alt=""
+							className="w-[50px] h-[50px] "
+						/>
+						<p>1 days ago</p>
+						<p className=" w-[30px] h-[25px] py-[2px] border text-center"> 1</p>
+						<p>C 100.0</p>
+						<p>C 200.0</p>
+						<MdOutlineDeleteOutline className=" bg-app-orange p-[2px] rounded-full text-center text-[20px] text-white hover:cursor-pointer" />
+					</div>
+
+					<div className="flex flex-col mt-[12px] w-full text-[13px]">
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Subtotal</p>
+							<p> Ȼ 300.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Discount</p>
+							<p> Ȼ 0.00</p>
+						</div>
+
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Shipping</p>
+							<p> Ȼ 0.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Taxes</p>
+							<p> Ȼ 5.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center font-semibold w-full mb-[3px]">
+							<p>Total</p>
+							<p> Ȼ 305.00</p>
+						</div>
 					</div>
 				</div>
-				<div> right</div>
+				<div className="rounded-lg  border outline-1 bg-background  py-[15px] w-[30%] ml-[2px]">
+					<div className="px-[10px] pb-[5px] border-b">
+						<p className="font-bold">Customer</p>
+						<p className="mt-[8px]">johndoe@gmail.com</p>
+					</div>
+					<div className="px-[10px] w-full border-b pb-[5px]">
+						<div className="flex w-full flex-row justify-between items-center mt-[10px] text-[12px]">
+							<p>Shipping Address</p>
+							<p className="bg-green-500 px-[5px] hover:cursor-pointer py-[2px] rounded-lg text-white">
+								Edit
+							</p>
+						</div>
+						<div className="text-[10px] mt-[-10px]">
+							<p className="font-bold mt-[25px]">John Doe</p>
+							<p className="text-text-gray">House No 813/40 To</p>
+							<p className="text-text-gray">Greater Accra</p>
+							<p className="text-text-gray">CG-738-3306</p>
+							<p className="text-text-gray">+233539831157</p>
+						</div>
+					</div>
+
+					<div className="px-[10px] w-full pb-[5px]">
+						<div className="flex w-full flex-row justify-between items-center mt-[10px] text-[12px]">
+							<p>Billing Address</p>
+							<p className="bg-green-500 px-[5px] hover:cursor-pointer py-[2px] rounded-lg text-white">
+								Edit
+							</p>
+						</div>
+						<div className="text-[10px] mt-[-10px]">
+							<p className="font-bold mt-[25px]">John Doe</p>
+							<p className="text-text-gray">House No 813/40 To</p>
+							<p className="text-text-gray">Greater Accra</p>
+							<p className="text-text-gray">CG-738-3306</p>
+							<p className="text-text-gray">+233539831157</p>
+						</div>
+					</div>
+				</div>
 			</div>
 
-			<div>
-				<div>b left</div>
-				<div> b right</div>
+			<div className="mt-[20px] flex flex-row justify-between w-full">
+				<div className="rounded-lg border bg-background p-[20px] w-[70%]  mr-[5px]">
+					<div className="flex flex-row justify-between items-center">
+						<div className="flex flex-row">
+							<p className="font-bold text-[12px]">Order Summary</p>
+							<p className="bg-yellow-400 rounded-lg text-[12px] p-[3px] ml-[20px]">
+								{" "}
+								Unpaid
+							</p>
+						</div>
+						<p className="bg-green-400 px-[6px] text-white rounded-lg text-[12px] py-[4px]">
+							Mark as paid
+						</p>
+					</div>
+
+					<div className="flex flex-col mt-[12px] w-full text-[13px]">
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Subtotal</p>
+							<p> Ȼ 300.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Discount</p>
+							<p> Ȼ 0.00</p>
+						</div>
+
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Shipping</p>
+							<p> Ȼ 0.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center w-full mb-[3px]">
+							<p>Taxes</p>
+							<p> Ȼ 5.00</p>
+						</div>
+						<div className="flex flex-row justify-between items-center font-semibold w-full mb-[3px]">
+							<p>Total</p>
+							<p> Ȼ 305.00</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="  flex flex-col   w-[30%] ml-[2px]">
+					<div className="rounded-lg bg-background border p-[15px] w-full ">
+						<p className="font-bold text-[12px]"> Sales Channel</p>
+						<p className="text-[12px] mt-[10px]">Channel - GHc</p>
+					</div>
+
+					<div className="rounded-lg bg-background border mt-[10px] p-[15px] w-full ">
+						<div className="w-full flex flex-row justify-between items-center">
+							<p className="font-bold text-[12px]"> Invoice</p>
+							<p className="text-[12px] border px-[5px] py-[3px] hover:cursor-pointer bg-white">
+								Generate
+							</p>
+						</div>
+
+						<p className="text-[12px] mt-[10px]">No Invoice to be shown</p>
+					</div>
+				</div>
 			</div>
-			<div></div>
+			<div className="mt-[10px] w-full border rounded-lg p-[20px] bg-background">
+				<p className="font-bold text-[12px]">Order History</p>
+				<div className="w-full flex flex-row mt-[10px]">
+					<Avatar className="mr-[10px]" />
+					<Space.Compact className="w-full">
+						<Input
+							className="w-full outline-none hover:outline-none"
+							placeholder="Leave your note here ..."
+						/>
+						<Button className="border outline-none ">Submit</Button>
+					</Space.Compact>
+				</div>
+				<div className="flex flex-row justify-between mt-[25px] w-full text-[12px]">
+					<div className="flex flex-row items-center">
+						<GoPrimitiveDot />
+						<p>AUTHORISATION</p>
+					</div>
+					<p className="align-end">3 days ago</p>
+				</div>
+				<div className="flex flex-row justify-between mt-[10px] w-full text-[12px]">
+					<div className="flex flex-row items-center">
+						<GoPrimitiveDot />
+						<p>Order was confirmed</p>
+					</div>
+					<p className="align-end">3 days ago</p>
+				</div>
+				<div className="flex flex-row justify-between mt-[10px] w-full text-[12px]">
+					<div className="flex flex-row items-center">
+						<GoPrimitiveDot />
+						<p>Order was placed</p>
+					</div>
+					<p className="align-end">3 days ago</p>
+				</div>
+			</div>
+			<div className="w-full rounded-lg mt-[20px] bg-background p-[25px] justify-end flex flex-row">
+				<button
+					className="border outline-1 
+                 px-[10px] py-[5px] rounded-lg mr-[10px]">
+					Back
+				</button>
+				<button className="bg-bright-blue text-white rounded-lg px-[5px] py-[3px]">
+					Confirm Order
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/components/DashboardProductPage.js
+++ src/components/DashboardProductPage.js
@@ -1,13 +1,36 @@
 import { Checkbox, Input, Rate } from "antd";
+import Dragger from "antd/es/upload/Dragger";
 import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsPencil } from "react-icons/bs";
-import { MdOutlineDeleteOutline } from "react-icons/md";
+import { MdDelete, MdOutlineDeleteOutline } from "react-icons/md";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { message } from "antd";
+import { FiUpload } from "react-icons/fi";
 
 const DashboardProductPage = () => {
 	const [search, setSearch] = useState("");
-	const [flip, setFlip] = useState(false);
+	const [page, setPage] = useState("all");
+
+	const props = {
+		name: "file",
+		multiple: true,
+		action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+		onChange(info) {
+			const { status } = info.file;
+			if (status !== "uploading") {
+				console.log(info.file, info.fileList);
+			}
+			if (status === "done") {
+				message.success(`${info.file.name} file uploaded successfully.`);
+			} else if (status === "error") {
+				message.error(`${info.file.name} file upload failed.`);
+			}
+		},
+		onDrop(e) {
+			console.log("Dropped files", e.dataTransfer.files);
+		}
+	};
 
 	const CategoryCard = ({ count, name, src, onClick }) => {
 		return (
@@ -28,27 +51,29 @@ const DashboardProductPage = () => {
 				<div className="flex flex-row justify-center items-center">
 					<p
 						className={`${
-							!flip ? "bg-bright-blue" : "bg-transparent"
+							page === "all" ? "bg-bright-blue" : "bg-transparent"
 						} px-[10px] py-[7px] ${
-							!flip ? "text-white" : "text-black"
+							page === "all" ? "text-white" : "text-black"
 						} rounded-lg hover:cursor-pointer `}
-						onClick={() => setFlip(false)}>
+						onClick={() => setPage("all")}>
 						All
 					</p>
 
 					<p
 						className={`ml-[20px] ${
-							flip ? "bg-bright-blue" : "bg-transparent"
+							page === "categories" ? "bg-bright-blue" : "bg-transparent"
 						} px-[10px] py-[7px] ${
-							flip ? "text-white" : "text-black"
+							page === "categories" ? "text-white" : "text-black"
 						} rounded-lg  hover:cursor-pointer`}
-						onClick={() => setFlip(true)}>
+						onClick={() => setPage("categories")}>
 						Categories
 					</p>
 				</div>
 
 				<div className="flex flex-row ">
-					<button className="bg-bright-blue w-[100px] rounded-xl  py-[8px] text-white">
+					<button
+						className="bg-bright-blue w-[100px] rounded-xl  py-[8px] text-white"
+						onClick={() => setPage("create")}>
 						{" "}
 						+ Add New
 					</button>
@@ -65,8 +90,7 @@ const DashboardProductPage = () => {
 					/>
 				</div>
 			</div>
-
-			{!flip ? (
+			{page === "all" && (
 				<div className="mt-[10px] bg-background">
 					<div className="border-b  p-[8px]">Manage Products</div>
 					<table className="w-full">
@@ -125,7 +149,8 @@ const DashboardProductPage = () => {
 						</tbody>
 					</table>
 				</div>
-			) : (
+			)}{" "}
+			{page === "categories" && (
 				<div className="  flex-wrap justify-start mt-[10px] grid grid-cols-4 gap-3">
 					<CategoryCard
 						name={"Alcoholic Drinks"}
@@ -167,6 +192,123 @@ const DashboardProductPage = () => {
 						count={23}
 						src="https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZHJpbmtzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
 					/>
+				</div>
+			)}
+			{page === "create" && (
+				<div className="rounded-lg mt-[10px] bg-white p-[15px] flex flex-row w-full">
+					<div className="w-[45%] mr-[10px] rounded-lg border p-[10px]">
+						<p>Add Image</p>
+						<div>
+							<Dragger {...props}>
+								<div className="flex flex-col justify-center max-w-full items-center">
+									<img
+										alt=""
+										src={"/assets/upload.svg"}
+										className="w-[50px] h-[50px]"
+									/>
+									<p className="flex flex-row items-center">
+										<FiUpload className="mr-[3px] text-bright-blue" /> Drop your
+										file here or{" "}
+										<span className="text-bright-blue ml-[3px]">Browse</span>
+									</p>
+								</div>
+							</Dragger>
+						</div>
+						<br />
+
+						<div className="bg-background w-full flex flex-row justify-between mb-[20px] px-[5px] items-center">
+							<div className="flex flex-row items-center">
+								<img
+									className="w-[50px] h-[50px] mr-[4px]"
+									alt=""
+									src="https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZHJpbmtzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+								/>
+
+								<div className="text-[12px]">
+									<p>Baileys Original 01.png</p>
+									<p>400kb</p>
+								</div>
+							</div>
+
+							<MdDelete className="text-red-500 text-[20px]" />
+						</div>
+						<div className="bg-background w-full flex flex-row justify-between mb-[20px] px-[5px] items-center">
+							<div className="flex flex-row items-center">
+								<img
+									className="w-[50px] h-[50px] mr-[4px]"
+									alt=""
+									src="https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZHJpbmtzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+								/>
+
+								<div className="text-[12px]">
+									<p>Baileys Original 01.png</p>
+									<p>400kb</p>
+								</div>
+							</div>
+
+							<MdDelete className="text-red-500 text-[20px]" />
+						</div>
+						<div className="bg-background w-full flex flex-row justify-between mb-[20px] px-[5px] items-center">
+							<div className="flex flex-row items-center">
+								<img
+									className="w-[50px] h-[50px] mr-[4px]"
+									alt=""
+									src="https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZHJpbmtzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+								/>
+
+								<div className="text-[12px]">
+									<p>Baileys Original 01.png</p>
+									<p>400kb</p>
+								</div>
+							</div>
+
+							<MdDelete className="text-red-500 text-[20px] hover:cursor-pointer" />
+						</div>
+					</div>
+					<div className=" w-[50%] ml-[10px] flex flex-col">
+						<div className=" w-full  p-[20px] rounded-lg border flex flex-col">
+							<label>Product Name</label>
+							<input className=" border rounded-lg outline-none mt-[4px]" />
+							<label className="mt-[15px]">Category Name</label>
+							<input className=" border rounded-lg outline-none mt-[4px]" />
+							<div className="flex flex-row w-full justify-between mt-[15px]">
+								<div className="w-[50%] mr-[3px] flex flex-col">
+									<label>Item Number</label>
+									<input className=" border rounded-lg outline-none mt-[4px]" />
+								</div>
+								<div className="w-[50%] ml-[3px] flex flex-col">
+									<label>Brand</label>
+									<input className=" border rounded-lg outline-none mt-[4px]" />
+								</div>
+							</div>
+
+							<div className="flex flex-row w-[100%] justify-between mt-[15px]">
+								<div className="w-[30%] mr-[3px] flex flex-col">
+									<label>Price</label>
+									<input className=" border rounded-lg outline-none mt-[4px]" />
+								</div>
+
+								<div className="w-[30%] mx-[3px] flex flex-col">
+									<label>Expiry</label>
+									<input className=" border rounded-lg outline-none mt-[4px]" />
+								</div>
+
+								<div className="w-[30%] ml-[3px] flex flex-col">
+									<label>Quantity</label>
+									<input className=" border rounded-lg outline-none mt-[4px]" />
+								</div>
+							</div>
+
+							<label className="mt-[15px]">Description</label>
+							<textarea className=" border rounded-lg outline-none mt-[4px]" />
+						</div>
+
+						<div className="flex justify-end w-full mt-[20px] rounded-lg">
+							<button className="bg-bright-blue w-[120px] py-[4px] text-white">
+								Add Item
+							</button>
+						</div>
+					</div>
 				</div>
 			)}
 		</div>
