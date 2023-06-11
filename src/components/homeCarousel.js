82151fb4677668bd391f15c93cf6a2cac3acfc9d
LakeCombs <adesolaolamilekan2@gmail.com>
2023-06-11 06:45:04 +0100

    carousel with images added

--- src/components/homeCarousel.js
+++ src/components/homeCarousel.js
@@ -5,30 +5,53 @@ import { Carousel } from "antd";
 const HomeCarousel = () => {
 	const navigate = useNavigate();
 	return (
-		<div className="flex h-[300px] w-full ">
-			<Carousel autoplay={true}>
-				<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col ">
-					<div className="h-full w-full bg-[url('../components/bg-images/MacBook Pro 14_ - 1bg.png')] bg-no-repeat">
-						hello
+		<>
+			<div className="w-full  bg-green-400 rounded-lg">
+				<Carousel autoplay={true}>
+					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col ">
+						<img
+							src={"/assets/MacBook Pro 14_ - 1bg.png"}
+							className="h-full w-full"
+						/>
+
+						<div className="h-full w-full z-10 mt-[-300px] flex justify-center items-center flex-col pl-[40px]">
+							<h1 className="text-[20px] text-white">
+								Save up to 20% This Christmas <br /> on items Purchased from our
+								shop
+							</h1>
+
+							<br />
+
+							<button className=" py-[5px] rounded-md px-[40px] bg-black text-white">
+								Order Now
+							</button>
+						</div>
 					</div>
-				</div>
-				<div className=" h-full md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col">
-					<div className="h-full w-full bg-[url('../components/bg-images/MacBook Pro 14_ - 1bg.png')] bg-no-repeat">
-						lake
+					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col ">
+						<img
+							src={"/assets/MacBook Pro 14_ - 1bg2.png"}
+							className="h-full w-full"
+						/>
+
+						<div className="h-full w-full ">lake</div>
+					</div>{" "}
+					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col bg-hero3 object-fit">
+						<img
+							src={"/assets/MacBook Pro 14_ - 1bg3.png"}
+							className="h-full w-full"
+						/>
+						<div className="h-full w-full ">combs</div>
+					</div>{" "}
+					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col bg-hero4 object-fit">
+						<img
+							src={"/assets/MacBook Pro 14_ - 1bg4 (1).png"}
+							className="h-full w-full"
+						/>
+						<div className="h-full w-full ">what up</div>
 					</div>
-				</div>
-				<div className=" h-full md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col">
-					<div className="h-full w-full bg-[url('../components/bg-images/MacBook Pro 14_ - 1bg.png')] bg-no-repeat">
-						whats
-					</div>
-				</div>
-				<div className=" h-full md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col">
-					<div className="h-full w-full bg-[url('../components/bg-images//MacBook Pro 14_ - 1bg.png')] bg-no-repeat">
-						up here
-					</div>
-				</div>
-			</Carousel>
-		</div>
+				</Carousel>
+			</div>
+		</>
 	);
 };
 
