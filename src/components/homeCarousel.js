5fa54d157cd792ad0fc59db5ba90fab60a30f57a
Adesola Olalekan <71498022+LakeCombs@users.noreply.github.com>
2023-06-11 15:34:52 +0100

    added carousel

--- src/components/homeCarousel.js
+++ src/components/homeCarousel.js
@@ -15,7 +15,7 @@ const HomeCarousel = () => {
 						/>
 
 						<div className="h-full w-full z-10 mt-[-300px] flex justify-center items-center flex-col pl-[40px]">
-							<h1 className="text-[20px] text-white">
+							<h1 className="text-[20px] text-white font-bold">
 								Save up to 20% This Christmas <br /> on items Purchased from our
 								shop
 							</h1>
@@ -32,22 +32,41 @@ const HomeCarousel = () => {
 							src={"/assets/MacBook Pro 14_ - 1bg2.png"}
 							className="h-full w-full"
 						/>
-
-						<div className="h-full w-full ">lake</div>
+						<div className="h-full w-full z-10 mt-[-300px] flex justify-center items-start flex-col pl-[40px]">
+							<p className="font-bold">
+								<span className=" text-[40px]">Same Day</span>{" "}
+								<span className=" text-[40px] text-white mt-[-30px]">
+									Delivery
+								</span>
+								<br />
+								on <span className="text-white">orders</span> within{" "}
+								<span className="text-white">Accra</span>
+							</p>
+						</div>
 					</div>{" "}
 					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col bg-hero3 object-fit">
 						<img
 							src={"/assets/MacBook Pro 14_ - 1bg3.png"}
 							className="h-full w-full"
 						/>
-						<div className="h-full w-full ">combs</div>
+
+						<div className="h-full w-full z-10 mt-[-300px] flex justify-center items-start flex-col pl-[70px]">
+							<p className="font-bold text-[40px]">
+								<span className="  text-white">Get all</span>
+								<br />
+								<span className="  text-white mt-[-30px]">
+									Daily Essentials
+								</span>
+								<br />
+								<span className="text-white">From</span> Bevet
+							</p>
+						</div>
 					</div>{" "}
 					<div className=" h-[400px] w-[400px] md:h-[335px] w-full bg-blue-400 rounded-xl flex flex-col bg-hero4 object-fit">
 						<img
 							src={"/assets/MacBook Pro 14_ - 1bg4 (1).png"}
 							className="h-full w-full"
 						/>
-						<div className="h-full w-full ">what up</div>
 					</div>
 				</Carousel>
 			</div>
