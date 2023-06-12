dfb56a50b9cf6cf3d052ecc5c7462b4dd10d0e40
Adesola Olalekan <71498022+LakeCombs@users.noreply.github.com>
2023-06-12 14:30:11 +0100

    added the order page

--- src/screens/orderScreen.js
+++ src/screens/orderScreen.js
@@ -20,40 +20,101 @@ const MyOrderScreen = () => {
 						<h3 className="font-bold family-poppins">My Orders</h3>
 					</div>
 
-					<table className="table-fixed">
-						<thead>
-							<tr>
-								<th className="px-4 py-2">Order id</th>
-								<th className="px-4 py-2">Product</th>
-								<th className="px-4 py-2">Qty</th>
-								<th className="px-4 py-2">Delivery Status</th>
-							</tr>
-						</thead>
-						<tbody>
-							{orders?.map((order) => (
-								<tr key={order?._id}>
-									<td className="border px-4 py-2">
-										{" "}
-										{order?._id?.slice(0, 5)}
-									</td>
-									<td className="border px-4 py-2">
-										<div className="flex flex-row">
-											<img className=" h-[60px] w-[60px]" alt="" />{" "}
-											<div className="ml-[5px]">
-												<p>Name: </p>
-												<p>Price: </p>
-												<p>Qty:</p>
-											</div>
-										</div>
-									</td>
+					<div className="container mx-auto px-4 py-8">
+						<div className="bg-white shadow-md rounded-lg overflow-x-auto">
+							<table className="min-w-full bg-white">
+								<thead>
+									<tr className="border-b">
+										<th className="px-6 py-4 text-left font-semibold uppercase">
+											Order id
+										</th>
+										<th className="px-6 py-4 text-left font-semibold uppercase">
+											Product
+										</th>
+										<th className="px-6 py-4 text-left font-semibold uppercase">
+											Total Cost
+										</th>
+										<th className="px-6 py-4 text-left font-semibold uppercase">
+											Delivery Status
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									{orders?.map((order) => (
+										<tr
+											key={order?._id}
+											className="border-b hover:bg-gray-100 hover:cursor-pointer">
+											<td className="border px-4 py-2">
+												{" "}
+												{order?._id?.slice(0, 5)}
+											</td>
+											<td className="px-6 py-4">
+												{order?.items?.map((ord) => {
+													return (
+														<div className="flex flex-row">
+															<img
+																className=" h-[60px] w-[60px]"
+																alt=""
+																src={ord?.product?.images[0]}
+															/>{" "}
+															<div className="ml-[5px]">
+																<p>Name: {ord?.product?.name} </p>
+																<p>Price:GHc {ord?.product?.price} </p>
+																<p>Qty: {ord?.qty}</p>
+															</div>
+														</div>
+													);
+												})}
+											</td>
 
-									<td className="border px-4 py-2">{}</td>
+											<td className="px-6 py-4">
+												GHc{" "}
+												{order?.items?.reduce((accumulator, currentValue) => {
+													console.log("the currentValue is ", currentValue);
 
-									<td className="border px-4 py-2">{order?.delivery_status}</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
+													return (
+														accumulator +
+														currentValue?.product?.price * currentValue?.qty
+													);
+												}, 0)}
+											</td>
+
+											<td
+												className={`px-6 py-4 ${
+													order?.delivery_status === "pending"
+														? "text-orange-500"
+														: ""
+												}  
+                                                
+                                                
+                                                ${
+																									order?.delivery_status ===
+																									"success"
+																										? "text-green-500"
+																										: ""
+																								}
+												
+                                                
+                                                
+
+
+                                                  ${
+																										order?.delivery_status ===
+																										"failed"
+																											? "text-red-500"
+																											: ""
+																									}
+												
+                                                
+                                                `}>
+												{order?.delivery_status}
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
+					</div>
 				</div>
 			</ScreenWithPadding>
 		</div>
